2c7386a8e626984562c8d63ae8bc29bfc8f05760
Marina Sidorkina <m.sidorkina55@gmail.com>
2019-04-20 17:06:12 +0300

    Add expiring time counter.

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-require_once "helpers.php";
+require_once "config/general.php";
 require_once "data/general.php";
 require_once "data/layout.php";
 require_once "data/main.php";
 require_once "lib/main.php";
+require_once "helpers.php";
 
 $content = include_template("main.php", [
     "categories" => $categories,
